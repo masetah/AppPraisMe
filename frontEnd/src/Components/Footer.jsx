96ef8becc12b66b21a1ae653b49d76f58e597a3b
Maysam Taherian <maysamtaherian@Maysams-MacBook-Pro.local>
2019-10-19 13:00:17 -0500

    removed front end README.md

--- frontEnd/src/Components/Footer.jsx
+++ frontEnd/src/Components/Footer.jsx
@@ -16,10 +16,10 @@ class Footer extends Component {
         <NavbarBrand href="/" className="mr-auto">AppPraise Me 2019</NavbarBrand>
             <Nav>
                 <NavItem>
-                    <NavLink className="logo" id="github" href="https://github.com/masetah/AppPraisMe" ><img src="/Logos/github-logo.png" alt="Github Logo"/> </NavLink> 
+                    <NavLink className="logo" id="github" href="https://github.com/masetah/AppPraisMe"  ><img src="/Logos/github-logo.png" alt="Github Logo"/> </NavLink> 
                 </NavItem>
                 <NavItem>
-                    <NavLink className="logo" id="linkedin" href="https://www.linkedin.com/in/masetaherian/" ><img src="/Logos/linkedin.png" alt="Linkedin Logo"/> </NavLink>
+                    <NavLink className="logo" id="linkedin" href="https://www.linkedin.com/in/masetaherian/"  ><img src="/Logos/linkedin.png" alt="Linkedin Logo"/> </NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink className="logo" id="facebook" href="https://www.facebook.com/mase.taherian" ><img src="/Logos/facebook.png" alt="Facebook Logo"/> </NavLink>
@@ -28,7 +28,7 @@ class Footer extends Component {
                     <NavLink className="logo" id="profileSite" href="/" ><img src="/Logos/web-page.png" alt="Webpage Logo"/> </NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink className="logo" id="resume" href="https://docs.google.com/document/d/10WLVziclEpZTavVlsRC3ToLlMVfBwFeCMBfGVLkJ7a4/edit?usp=sharing" ><img src="/Logos/resume.png" alt="Resume Logo"/> </NavLink>
+                    <NavLink className="logo" id="resume" href="https://docs.google.com/document/d/10WLVziclEpZTavVlsRC3ToLlMVfBwFeCMBfGVLkJ7a4/edit?usp=sharing" target="_blank"><img src="/Logos/resume.png" alt="Resume Logo"/> </NavLink>
                 </NavItem>
             </Nav>
         </Navbar>
